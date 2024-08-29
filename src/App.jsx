9c5ac35f930b4a8986195480ad0f6f319728785b
ReetSolanki <135243222+ReetSolanki@users.noreply.github.com>
2024-08-30 01:00:59 +0530

    Update App.jsx
    THis is updatedd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './home';
 import Chatbot from './chatbot';
 
